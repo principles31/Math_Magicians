476e16ef74ccb6ff7e103912a4158fb260a50ac0
principles31 <principles31@gmail.com>
2022-06-14 15:40:14 +0000

    Add components files

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,31 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Keys from './Keys';
+import calculate from '../logic/calculator';
 
-class Calcu extends React.PureComponent {
-  render() {
-    return (
-      <div className="calculatorDisplay">
-        <div className="result">0</div>
-        <div className="operand gry">AC</div>
-        <div className="operand gry">&#43;/&minus;</div>
-        <div className="operand gry">%</div>
-        <div className="operand op">&divide;</div>
-        <div className="digits gry">7</div>
-        <div className="digits gry">8</div>
-        <div className="digits gry">9</div>
-        <div className="digits op">&times;</div>
-        <div className="digits gry">4</div>
-        <div className="digits gry">5</div>
-        <div className="digits gry">6</div>
-        <div className="digits op">-</div>
-        <div className="digits gry">2</div>
-        <div className="digits gry">3</div>
-        <div className="digits op">+</div>
-        <div className="digits span gry">0</div>
-        <div className="digits gry">.</div>
-        <div className="digits gry">=</div>
-      </div>
-    );
+const Calcu = () => {
+  const [state, setState] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
+  const handleEvent = (e) => {
+    const oprObject = calculate(state, e.target.textContent);
+    setState(oprObject);
+  };
+
+  const { total, operation, next } = state;
+  const oprnd = operation === '%' ? 'mod' : operation;
+  let result = '';
+  if (total) {
+    result = `${total} ${oprnd || ''} ${next || ''}`;
+  } else if (next) {
+    result = `${next} ${oprnd || ''}`;
   }
-}
+  return (
+    <div className="calculator-dis">
+      <p className="math-intro">Lets do some Math!</p>
+      <div className="calculatorDisplay">
+
+        <div className="result">{result || 0 }</div>
+        <Keys styles="operand gry" handleEvent={(e) => handleEvent(e)} val="AC" />
+        <Keys styles="operand gry" handleEvent={(e) => handleEvent(e)} val="+/-" />
+        <Keys styles="operand gry" handleEvent={(e) => handleEvent(e)} val="%" />
+        <Keys styles="operand op" handleEvent={(e) => handleEvent(e)} val="÷" />
+        <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="7" />
+        <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="8" />
+        <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="9" />
+        <Keys styles="digits op" handleEvent={(e) => handleEvent(e)} val="x" />
+        <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="4" />
+        <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="5" />
+        <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="6" />
+        <Keys styles="digits op" handleEvent={(e) => handleEvent(e)} val="-" />
+        <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="1" />
+        <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="2" />
+        <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="3" />
+        <Keys styles="digits op" handleEvent={(e) => handleEvent(e)} val="+" />
+        <Keys styles="digits span gry" handleEvent={(e) => handleEvent(e)} val="0" />
+        <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="." />
+        <Keys styles="digits op" handleEvent={(e) => handleEvent(e)} val="=" />
+      </div>
+    </div>
+  );
+};
 
 export default Calcu;
